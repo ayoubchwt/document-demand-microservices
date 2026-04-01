2d196e939ac84d19a3c33fb766e4c61ddc7dcde7
eclipse <ayoub.chouat@esprit.tn>
2026-04-01 17:08:42 +0100

    fixed the cors problem when by adding cors annotation to the microservices controllers

--- Services/payment-microservice/src/main/java/tn/citypulse/paymentmicroservice/controller/PaymentController.java
+++ Services/payment-microservice/src/main/java/tn/citypulse/paymentmicroservice/controller/PaymentController.java
@@ -11,6 +11,7 @@ import tn.citypulse.paymentmicroservice.Service.IPaymentService;
 
 @RestController
 @RequestMapping("/payment")
+@CrossOrigin("http://localhost:8087")
 @RequiredArgsConstructor
 @Tag(name = "Payments", description = "Endpoints for managing Stripe payment sessions and secure webhooks")
 public class PaymentController {
