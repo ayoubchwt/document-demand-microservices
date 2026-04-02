f88ce7c76002b96de5b3d8c8d70c9b1811b51d87
eclipse <ayoub.chouat@esprit.tn>
2026-04-02 18:10:20 +0100

    added ngrok to the cluster and configured all its stuff

---
+++ Services/payment-microservice/src/main/java/tn/citypulse/paymentmicroservice/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package tn.citypulse.paymentmicroservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8087");
+        server.setDescription("API Gateway");
+        return new OpenAPI().servers(List.of(server));
+    }
+}
